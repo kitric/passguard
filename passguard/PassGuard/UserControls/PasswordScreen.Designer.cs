6b2054eb13d65eb360e0cd8317fc7b7b363f16c1
Diogo <diogo6204@gmail.com>
2021-01-10 16:40:24 +0000

    Update PasswordScreen.Designer.cs

--- passguard/PassGuard/UserControls/PasswordScreen.Designer.cs
+++ passguard/PassGuard/UserControls/PasswordScreen.Designer.cs
@@ -206,7 +206,6 @@ namespace PassGuard.UserControls
             // 
             // InvalidNameLB
             // 
-            this.InvalidNameLB.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.InvalidNameLB.AutoSize = true;
             this.InvalidNameLB.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.InvalidNameLB.ForeColor = System.Drawing.Color.Red;
