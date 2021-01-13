c246f2bb1ce73154f41e8ca20a34472b4fbf27f3
Diogo <diogo6204@gmail.com>
2021-01-13 10:01:29 +0000

    added a few settings.
    There's no way to interact with them though.
    
    Co-Authored-By: Tanveer Najib <48864420+crxssed7@users.noreply.github.com>

--- passguard/PassGuard/UserControls/SettingsPage.Designer.cs
+++ passguard/PassGuard/UserControls/SettingsPage.Designer.cs
@@ -29,19 +29,49 @@ namespace PassGuard.UserControls
         /// </summary>
         private void InitializeComponent()
         {
+            this.ThemeLB = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // ThemeLB
+            // 
+            this.ThemeLB.AutoSize = true;
+            this.ThemeLB.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ThemeLB.ForeColor = System.Drawing.Color.White;
+            this.ThemeLB.Location = new System.Drawing.Point(71, 24);
+            this.ThemeLB.Name = "ThemeLB";
+            this.ThemeLB.Size = new System.Drawing.Size(47, 17);
+            this.ThemeLB.TabIndex = 0;
+            this.ThemeLB.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Theme:";
             // 
             // SettingsPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(33)))), ((int)(((byte)(75)))));
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ThemeLB);
             this.Name = "SettingsPage";
             this.Size = new System.Drawing.Size(640, 450);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ThemeLB;
+        private System.Windows.Forms.Label label1;
     }
 }
