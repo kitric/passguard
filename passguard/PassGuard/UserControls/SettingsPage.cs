7972395e17bf98988ec854b6ff2532fc477a8135
Diogo <diogo6204@gmail.com>
2021-01-14 17:39:41 +0000

    Update SettingsPage.cs
    Co-Authored-By: Tanveer Najib <48864420+crxssed7@users.noreply.github.com>

--- passguard/PassGuard/UserControls/SettingsPage.cs
+++ passguard/PassGuard/UserControls/SettingsPage.cs
@@ -18,19 +18,14 @@ namespace PassGuard.UserControls
             this.ThemeLB.Text = MainScreen.Data.Theme.ToString().ToLower().Replace("_", " ");
         }
 
-        private void btn1_Click(object sender, System.EventArgs e)
+        private void btn1_Click(object sender, EventArgs e)
         {
             int current = (int)Enum.Parse(typeof(Theme), this.ThemeLB.Text.ToUpper().Replace(" ", "_"));
 
-            if (current == 0) { 
-                // Select the last element of the enum.
-                current = Enum.GetValues(typeof(Theme)).Length - 1;
             
-            } else
-            {
-                // Select the theme above the current one.
-                --current; 
-            }
+            // If the current theme is the first one select the last one
+            // otherwise select the theme above the current one.
+            current = current != 0 ? --current : Enum.GetValues(typeof(Theme)).Length - 1;
 
             Theme temp = (Theme)current;
             this.ThemeLB.Text = temp.ToString().ToLower().Replace("_", " ");
@@ -40,16 +35,10 @@ namespace PassGuard.UserControls
         private void btn2_Click(object sender, EventArgs e)
         {
             int current = (int)Enum.Parse(typeof(Theme), this.ThemeLB.Text.ToUpper().Replace(" ", "_"));
-            
-            if (current == Enum.GetValues(typeof(Theme)).Length - 1) //The last one
-            {
-                current = 0;
-            
-            } else
-            {
-                // Select the theme below the current one.
-                current++;
-            }
+
+            // If the current theme is the last one select the first one
+            // otherwise select the theme below the current one.
+            current = current == Enum.GetValues(typeof(Theme)).Length - 1 ? 0 : ++current;
 
             Theme temp = (Theme)current;
             this.ThemeLB.Text = temp.ToString().ToLower().Replace("_", " ");
